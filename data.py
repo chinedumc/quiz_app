49892deb7388bbdcdbb623c7143e5aaa9eadb871
chinedumc <chibokachinedum@gmail.com>
2025-10-24 23:36:45 +0100

    Quiz_App

---
+++ data.py
@@ -0,0 +1,13 @@
+question_data = [
+    {"text": "A slug's blood is green.", "answer": "True"},
+    {"text": "The Great Wall of China is visible from space with the naked eye.", "answer": "False"},
+    {"text": "Bananas grow on trees.", "answer": "False"},
+    {"text": "Lightning never strikes the same place twice.", "answer": "False"},
+    {"text": "The human body has four lungs.", "answer": "False"},
+    {"text": "Goldfish only have a memory span of three seconds.", "answer": "False"},
+    {"text": "The capital of Australia is Sydney.", "answer": "False"},
+    {"text": "Octopuses have three hearts.", "answer": "True"},
+    {"text": "Sound travels faster in water than in air.", "answer": "True"},
+    {"text": "The Sahara Desert is the largest desert in the world.", "answer": "False"},
+    {"text": "Some cats are allergic to humans.", "answer": "True"}
+]
